31ba16c52b375ddff4032d0845beec8eee9e5e90
lxynox <lxynox@gmail.com>
2018-01-06 15:35:21 -0500

    Code split with react-loadable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import Loadable from 'react-loadable';
+const App = Loadable.Map({
+  loader: {
+    app1: () => import(/* webpackChunkName: 'foo' */'./App1.js'),
+    app2: () => import(/* webpackChunkName: 'bar' */'./App2.js'),
+    app3: () => import(/* webpackChunkName: 'baz' */'./App3.js'),
+  },
+  loading: () => (<div>...LOADING...</div>),
+  render(loaded, props) {
+    return (
+      <div>
+      {Object.keys(loaded).map((name, index) => {
+        const Component = loaded[name].default;
+        return <Component key={`loaded-component-${index}`} />;
+      })}
+      </div>
+    )
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
